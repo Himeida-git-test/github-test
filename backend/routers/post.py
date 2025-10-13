c1bdad1a1170afc41b0c14c73d33a803f6c6ddcd
TGis <arititute@gmail.com>
2025-10-13 17:00:19 +0900

    [FEAT]home.jsx, post.py 추가

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,2 +1,8 @@
-print("post")
-print("post")
+from fastapi import APIRouter
+
+router=APIRouter(prefix='/post', tags=["post"])
+
+@router.get('/')
+def home() :
+    print("Hello World")
+    return {"msg" : 'Hello'} 
