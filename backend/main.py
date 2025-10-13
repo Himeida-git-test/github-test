806d51a1eee7c01ad2d1e7ab4d6abab55e3652de
faust <leejaechul92@gmail.com>
2025-10-13 15:17:33 +0900

    user database

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
+from fastapi import FastAPI
+from database import get_db, create_tables
 
+app = FastAPI()
+
+@app.get('/')
+def main():
+    return "home"
+
+@app.on_event("startup")
+def startup_event():
+    create_tables()
+    
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app",
+                host="127.0.0.1",
+                port=8000,
+                reload=True)
